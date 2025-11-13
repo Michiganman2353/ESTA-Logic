458d9000d18664515448aecd42dc4f763b7bf459
Michiganman2353 <Hankins.dan@sjncanton.org>
2025-11-13 08:49:26 -0500

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home'; // Your components
+import SignIn from './components/SignIn';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
